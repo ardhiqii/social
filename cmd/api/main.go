a1e699304755d2a1286febb3ce49826f155130f9
ardhiqii <ardhiqi@gmail.com>
2025-03-27 10:16:34 +0700

    adding env

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,21 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/ardhiqii/social/internal/env"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+
+	err := godotenv.Load("../../.env")
+	if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR",":8080"),
 	}
 	app := &application{
 		config: cfg,
