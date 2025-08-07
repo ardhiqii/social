0e27a80865b451a9d100dd4d42f03dd47cb7b31f
ardhiqii <ardhiqi@gmail.com>
2025-08-07 23:59:42 +0700

    add create comment api

--- internal/store/comments.go
+++ internal/store/comments.go
@@ -11,8 +11,7 @@ type Comment struct {
 	UserID    int64  `json:"user_id"`
 	Content   string `json:"content"`
 	CreatedAt string `json:"created_at"`
-	User      User   `json:"user"`
-
+	User      *User  `json:"user"`
 }
 
 type CommentStore struct {
@@ -29,8 +28,8 @@ func (s *CommentStore) GetByPostID(ctx context.Context, postID int64) ([]Comment
 	WHERE c.post_id = $1
 	ORDER BY c.created_at DESC;
 	`
-  ctx,cancel := context.WithTimeout(ctx,QueryTimeoutDuration)
-  defer cancel()
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
 
 	rows, err := s.db.QueryContext(ctx, query, postID)
 
@@ -41,18 +40,17 @@ func (s *CommentStore) GetByPostID(ctx context.Context, postID int64) ([]Comment
 	defer rows.Close()
 
 	comments := []Comment{}
-  for rows.Next(){
-    var c Comment
-    c.User = User{}
-    err := rows.Scan(&c.ID, &c.PostID, &c.UserID, &c.Content, &c.CreatedAt, &c.User.Username, &c.User.ID)
-    if err != nil{
-      return nil, err
-    }
-    comments = append(comments, c)
-  }
-  return comments, nil
+	for rows.Next() {
+		var c Comment
+		c.User = &User{}
+		err := rows.Scan(&c.ID, &c.PostID, &c.UserID, &c.Content, &c.CreatedAt, &c.User.Username, &c.User.ID)
+		if err != nil {
+			return nil, err
+		}
+		comments = append(comments, c)
+	}
+	return comments, nil
 }
-
 
 func (s *CommentStore) Create(ctx context.Context, comment *Comment) error {
 	query := `
@@ -60,12 +58,12 @@ func (s *CommentStore) Create(ctx context.Context, comment *Comment) error {
 	VALUES ($1,$2,$3)
 	RETURNING id, created_at
 	`
-	ctx,cancel := context.WithTimeout(ctx,QueryTimeoutDuration)
-  defer cancel()
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
 
-	err := s.db.QueryRowContext(ctx,query,comment.PostID, comment.UserID, comment.Content).Scan(&comment.ID, &comment.CreatedAt)
+	err := s.db.QueryRowContext(ctx, query, comment.PostID, comment.UserID, comment.Content).Scan(&comment.ID, &comment.CreatedAt)
 
-	if err != nil{
+	if err != nil {
 		return nil
 	}
 	return nil
